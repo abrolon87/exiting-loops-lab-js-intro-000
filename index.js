dbe2706895830d99e39870cb251432c90922d5b5
Amanda R <abrolon87@gmail.com>
2019-08-22 23:09:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function breakOut(array, changeValue, stopValue) {
   for (let i = 0; i < array.length; i++) {
   if (typeof array[i] !== stopValue) {
-    break
-  }
+    break;
+  }}
 }
   doSomethingToString(array[i])
 }
